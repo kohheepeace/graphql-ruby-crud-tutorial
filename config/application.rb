4db77413dfca9d50350c0abc660e435a71abcca6
kohheepeace <kohheepeace@gmail.com>
2019-03-09 19:18:06 +0800

    Finish Rails App Setup

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module RailsApiGraphqlCrudTuto
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.generators do |g| 
+      # don't generate controller spec when generate controller for api
+      g.controller_specs false
+
+      # auto generate facory when run 'rails g model' command
+      g.fixture_replacement :factory_bot, dir: 'spec/factories' 
+    end
   end
 end
