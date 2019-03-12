182d0b064ea045ac220deba3c15b345e82de6045
kohheepeace <kohheepeace@gmail.com>
2019-03-12 22:35:37 +0800

    Add validation and has_many to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,5 +19,7 @@
 #
 
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@
 #
 
 class User < ApplicationRecord
+  validates :name, presence: true
+  has_many :posts, dependent: :destroy
 end
